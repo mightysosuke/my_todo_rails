535c0a493c7bde5b974e9d92a544f71121b734b0
mightysosuke <sosuke.sin@gmail.com>
2022-08-15 18:08:02 +0900

    add: ユーザー、プロフィールテーブル

---
+++ db/schemas/users.schema.rb
@@ -0,0 +1,6 @@
+create_table :users, force: :cascade, charset: 'utf8mb4', options: 'ENGINE=InnoDB ROW_FORMAT=DYNAMIC' do |t|
+  t.string  :auth0_uid, null: false, comment: "Auth0ID"
+
+  t.datetime :updated_at, null: false, comment: "更新日時"
+  t.datetime :created_at, null: false, comment: "作成日時"
+end
