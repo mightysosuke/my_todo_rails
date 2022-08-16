b997a1be963405412e90c892a54c75ca2d1ae163
mightysosuke <sosuke.sin@gmail.com>
2022-08-16 14:51:50 +0900

    fix: アイコンの削除

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -6,7 +6,6 @@
 #  user_id    :string(255)      not null
 #  nickname   :string(255)      not null
 #  email      :string(255)      not null
-#  icon       :string(255)      not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
@@ -16,7 +15,6 @@ RSpec.describe Profile, type: :model do
   describe "validation" do
     it { is_expected.to validate_presence_of(:nickname) }
     it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_presence_of(:icon) }
   end
 
   describe "association" do
