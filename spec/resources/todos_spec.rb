f46b8c8dc2510b51c94014425234cb04951f00ac
mightysosuke <sou32suke@gmail.com>
2024-01-05 18:58:00 +0900

    add: user_me resource spec

--- spec/resources/todos_spec.rb
+++ spec/resources/todos_spec.rb
@@ -9,9 +9,8 @@ RSpec.describe TodosResource, type: :resource do
   describe "Resourceを使用した場合" do
     context "正常なオブジェクトを作成した場合" do
       it "オブジェクトのプロパティがTodoのプロパティと同じであること" do
-        expect(subject.object.id).to eq(todo.id)
-    expect(subject.object.title).to eq(todo.title)
-    expect(subject.object.content).to eq(todo.content)
+        expect(subject.object.title).to eq(todo.title)
+        expect(subject.object.content).to eq(todo.content)
       end
     end
   end
