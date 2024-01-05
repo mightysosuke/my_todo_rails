33472755c1c3771c9efab94bc60a126dc3e34ab6
mightysosuke <sou32suke@gmail.com>
2024-01-05 18:52:47 +0900

    add: resourcespec

---
+++ spec/resources/todos_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe TodosResource, type: :resource do
+  let!(:user) { create(:user) }
+  let!(:todo) { create(:todo, user: user) }
+
+  subject { described_class.new(todo) }
+
+  describe "Resourceを使用した場合" do
+    context "正常なオブジェクトを作成した場合" do
+      it "オブジェクトのプロパティがTodoのプロパティと同じであること" do
+        expect(subject.object.id).to eq(todo.id)
+    expect(subject.object.title).to eq(todo.title)
+    expect(subject.object.content).to eq(todo.content)
+      end
+    end
+  end
+end
