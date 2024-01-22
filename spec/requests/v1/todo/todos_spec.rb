b52ec70e994d86218de326851b6122e4e64263ab
mightysosuke <sou32suke@gmail.com>
2024-01-22 14:59:43 +0900

    update: リクエストスペック

--- spec/requests/v1/todo/todos_spec.rb
+++ spec/requests/v1/todo/todos_spec.rb
@@ -45,6 +45,43 @@ RSpec.describe "V1::Todo::Todos", type: :request do
     end
   end
 
+  describe "GET /v1/todo/todos/:id" do
+    let(:todo) { todos.first }
+
+    context "ログイン済みのユーザの場合" do
+      before do
+        authorization_stub
+        get v1_todo_path(todo.id), headers: headers
+      end
+
+      it "ステータスコードが200であること" do
+        expect(response).to have_http_status(200)
+      end
+
+      it "指定したtodoがJSON形式で返されること" do
+        json = JSON.parse(response.body)
+        expect(json['id']).to eq(todo.id)
+        expect(json['title']).to eq(todo.title)
+        expect(json['content']).to eq(todo.content)
+      end
+    end
+
+    context "ログインしていないユーザの場合" do
+      before do
+        get v1_todo_path(todo.id), headers: headers
+      end
+
+      it "ステータスコードが401であること" do
+        expect(response).to have_http_status(401)
+      end
+
+      it "エラーメッセージがJSON形式で返されること" do
+        json = JSON.parse(response.body)
+        expect(json["message"]).to eq("Unauthorized")
+      end
+    end
+  end
+
   describe "POST /v1/todos" do
     let(:valid_attributes) { attributes_for(:todo) }
     context "ログイン済みのユーザの場合" do
@@ -60,6 +97,21 @@ RSpec.describe "V1::Todo::Todos", type: :request do
         expect(user.todos.count).to eq(4)
         expect(user.todos.last.title).to eq(valid_attributes[:title])
         expect(user.todos.last.content).to eq(valid_attributes[:content])
+      end
+    end
+
+    context "ログインしていないユーザの場合" do
+      before do
+        post v1_todos_path, params: { todo: valid_attributes }.to_json, headers: headers
+      end
+
+      it "ステータスコードが401であること" do
+        expect(response).to have_http_status(401)
+      end
+
+      it "エラーメッセージがJSON形式で返されること" do
+        json = JSON.parse(response.body)
+        expect(json["message"]).to eq("Unauthorized")
       end
     end
   end
